af648d9135532bed36e348bfd26d1a5c4d4abe7a
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2019-11-10 14:47:50 +0300

    ProcedureRepository

---
+++ backend/src/main/java/com/tsystems/javaschool/medical/backend/dao/ProcedureRepository.java
@@ -0,0 +1,72 @@
+package com.tsystems.javaschool.medical.backend.dao;
+
+import com.tsystems.javaschool.medical.backend.dto.ProceduresDto;
+import com.tsystems.javaschool.medical.backend.entities.ProceduresEntity;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.tsystems.javaschool.medical.backend.util.DateUtils.getCurrentTimestamp;
+
+@Repository
+public class ProcedureRepository {
+
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional
+    public List<ProceduresEntity> getAll() {
+
+        Session session = sessionFactory.getCurrentSession();
+        Criteria criteria = session.createCriteria(ProceduresEntity.class);
+        criteria.addOrder(Order.asc("description"));
+        criteria.add(Restrictions.eq("deleted", "N"));
+        List<ProceduresEntity> proceduresEntities = criteria.list();
+
+        return proceduresEntities;
+    }
+
+    @Transactional
+    public void create(String description) {
+        ProceduresEntity proceduresEntity = new ProceduresEntity();
+        proceduresEntity.setDescription(description);
+        proceduresEntity.setCreatedAt(getCurrentTimestamp());
+        proceduresEntity.setUpdatedAt(getCurrentTimestamp());
+        proceduresEntity.setDeleted("N");
+
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(proceduresEntity);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        ProceduresEntity proceduresEntity = session.load(ProceduresEntity.class, id);
+        proceduresEntity.setDeleted("Y");
+        proceduresEntity.setUpdatedAt(getCurrentTimestamp());
+        proceduresEntity.setId(id);
+        session.update(proceduresEntity);
+    }
+
+    @Transactional
+    public void update(ProceduresDto params) {
+        Session session = sessionFactory.getCurrentSession();
+        ProceduresEntity proceduresEntity = session.load(ProceduresEntity.class, params.getId());
+        proceduresEntity.setUpdatedAt(getCurrentTimestamp());
+        proceduresEntity.setId(params.getId());
+        proceduresEntity.setDescription(params.getDescription());
+        session.update(proceduresEntity);
+    }
+
+}
--- backend/src/main/java/com/tsystems/javaschool/medical/backend/services/ProceduresService.java
+++ backend/src/main/java/com/tsystems/javaschool/medical/backend/services/ProceduresService.java
@@ -1,12 +1,8 @@
 package com.tsystems.javaschool.medical.backend.services;
 
+import com.tsystems.javaschool.medical.backend.dao.ProcedureRepository;
 import com.tsystems.javaschool.medical.backend.dto.ProceduresDto;
 import com.tsystems.javaschool.medical.backend.entities.ProceduresEntity;
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,68 +10,34 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.tsystems.javaschool.medical.backend.util.DateUtils.getCurrentTimestamp;
-
 @Service
 public class ProceduresService {
     @Autowired
     private ModelMapper modelMapper;
 
     @Autowired
-    private SessionFactory sessionFactory;
+    ProcedureRepository procedureRepository;
 
     public List<ProceduresDto> getProceduresList() {
 
-        Session session = sessionFactory.openSession();
-        session.getTransaction().begin();
-        Criteria criteria = session.createCriteria(ProceduresEntity.class);
-        criteria.addOrder(Order.asc("description"));
-        criteria.add(Restrictions.eq("deleted", "N"));
-        List list = criteria.list();
+        List<ProceduresEntity> proceduresEntities = procedureRepository.getAll();
         List<ProceduresDto> result = new ArrayList<>();
-        for (Object a : list) {
-            ProceduresDto proceduresDto = modelMapper.map(a, ProceduresDto.class);
+        for (Object entity : proceduresEntities) {
+            ProceduresDto proceduresDto = modelMapper.map(entity, ProceduresDto.class);
             result.add(proceduresDto);
         }
-        session.getTransaction().commit();
-        session.close();
         return result;
     }
 
     public void addProcedure(String description) {
-        ProceduresEntity proceduresEntity = new ProceduresEntity();
-        proceduresEntity.setDescription(description);
-        proceduresEntity.setCreatedAt(getCurrentTimestamp());
-        proceduresEntity.setUpdatedAt(getCurrentTimestamp());
-        proceduresEntity.setDeleted("N");
-        Session session = sessionFactory.openSession();
-        session.getTransaction().begin();
-        session.persist(proceduresEntity);
-        session.getTransaction().commit();
-        session.close();
+        procedureRepository.create(description);
     }
 
     public void deleteProcedure(int id) {
-        Session session = sessionFactory.openSession();
-        session.getTransaction().begin();
-        ProceduresEntity proceduresEntity = session.load(ProceduresEntity.class, id);
-        proceduresEntity.setDeleted("Y");
-        proceduresEntity.setUpdatedAt(getCurrentTimestamp());
-        proceduresEntity.setId(id);
-        session.update(proceduresEntity);
-        session.getTransaction().commit();
-        session.close();
+        procedureRepository.delete(id);
     }
 
     public void updateProcedure(ProceduresDto params) {
-        Session session = sessionFactory.openSession();
-        session.getTransaction().begin();
-        ProceduresEntity proceduresEntity = session.load(ProceduresEntity.class, params.getId());
-        proceduresEntity.setUpdatedAt(getCurrentTimestamp());
-        proceduresEntity.setId(params.getId());
-        proceduresEntity.setDescription(params.getDescription());
-        session.update(proceduresEntity);
-        session.getTransaction().commit();
-        session.close();
+        procedureRepository.update(params);
     }
 }
