4c324af9b0e767fe761a5a84d8b459b4c8ae5db5
anton.goloschapov <anton.goloschapov@rtlabs.ru>
2019-12-13 14:02:49 +0300

    Random stuffId

--- backend/src/main/java/com/tsystems/javaschool/medical/backend/services/PrescriptionCronService.java
+++ backend/src/main/java/com/tsystems/javaschool/medical/backend/services/PrescriptionCronService.java
@@ -2,8 +2,10 @@ package com.tsystems.javaschool.medical.backend.services;
 
 import com.tsystems.javaschool.medical.backend.component.enums.EventStatus;
 import com.tsystems.javaschool.medical.backend.dao.EventRepository;
+import com.tsystems.javaschool.medical.backend.dao.StaffRepository;
 import com.tsystems.javaschool.medical.backend.dto.EventRequestDto;
 import com.tsystems.javaschool.medical.backend.dto.prescriptions.PrescriptionRequestDto;
+import com.tsystems.javaschool.medical.backend.entities.StaffEntity;
 import org.quartz.CronExpression;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +15,16 @@ import java.time.Period;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class PrescriptionCronService {
     private final EventRepository eventRepository;
+    private final StaffRepository staffRepository;
 
-    public PrescriptionCronService(EventRepository eventRepository) {
+    public PrescriptionCronService(EventRepository eventRepository, StaffRepository staffRepository) {
         this.eventRepository = eventRepository;
+        this.staffRepository = staffRepository;
     }
 
     void generateEventsByPrescription(PrescriptionRequestDto prescriptionParams) {
@@ -27,19 +32,22 @@ public class PrescriptionCronService {
         ArrayList<Date> timeSet = new ArrayList<>();
         Period period = Period.between(prescriptionParams.getStartDate().toLocalDateTime().toLocalDate(), prescriptionParams.getEndDate().toLocalDateTime().toLocalDate());
         int diff = period.getDays();
+        List<StaffEntity> staffEntities = staffRepository.getAll();
+        int staffIndex = (int) (Math.random() * ((staffEntities.size()) + 1));
+        int stuffId = staffEntities.get(staffIndex).getId();
         try {
             for (long i = diff; i > 0; i--) {
                 CronExpression cron = new CronExpression(prescriptionParams.getTimePattern());
                 final Date firstExecution = cron.getNextValidTimeAfter(timeSet.isEmpty() ? prescriptionParams.getStartDate() : timeSet.get(timeSet.size() - 1));
                 timeSet.add(new Date(firstExecution.getTime()));
-                eventRepository.create(prepareEventRequestDto(firstExecution.getTime(), prescriptionParams));
+                eventRepository.create(prepareEventRequestDto(firstExecution.getTime(), prescriptionParams, stuffId));
             }
         } catch (ParseException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
     }
 
-    private static EventRequestDto prepareEventRequestDto(final long startDate, PrescriptionRequestDto prescriptionParams) {
+    private static EventRequestDto prepareEventRequestDto(final long startDate, PrescriptionRequestDto prescriptionParams, final int stuffId) {
 
 
         Calendar now = Calendar.getInstance();
@@ -53,7 +61,7 @@ public class PrescriptionCronService {
         eventRequestDto.setStatus(EventStatus.ASSIGNED.getValue());
         eventRequestDto.setPatientId(prescriptionParams.getPatientId());
         eventRequestDto.setProcedureId(prescriptionParams.getProcedureId());
-        eventRequestDto.setStaffId(1);
+        eventRequestDto.setStaffId(stuffId);
         eventRequestDto.setRoomId(1);
 
         return eventRequestDto;
